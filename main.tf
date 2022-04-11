1da5330cd1821055b3ae5b44e7f7c894f8a91c9a
Andrew Brudnak <brudnak@protonmail.com>
2022-04-10 22:47:18 -0700

    🔒 Moving variables to file

--- main.tf
+++ main.tf
@@ -26,32 +26,3 @@ resource "rancher2_secret" "rancher2_secret" {
     tf-secret-string2 = base64encode("topsecret-two-${count.index}")
   }
 }
-
-# Variable section
-
-variable "rancher_url" {
-  type        = string
-  description = "URL for Rancher."
-}
-
-variable "rancher_token_key" {
-  type        = string
-  description = "API bearer token for Rancher."
-  sensitive   = true
-}
-
-variable "project_id" {
-  type        = string
-  description = "The project ID where you want to create the secret."
-}
-
-variable "namespace_id" {
-  type        = string
-  description = "The namespace ID where you want to create the secret."
-}
-
-variable "secret_count" {
-  type        = number
-  description = "Amount of secrets that you would like to create within Rancher."
-}
-
